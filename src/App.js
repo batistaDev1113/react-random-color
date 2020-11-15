07468d4c1b22d6b1a9d8b0a1e7c9a9d546a2604f
yundeveloper <yuniorbatista1113@gmail.com>
2020-11-15 14:32:36 -0500

    fixed vulnerabilities in package.json and added some CSS

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button} from './components/Button';
+import { Button } from './components/Button';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      color: [232, 25, 180]
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			color : [
+				232,
+				25,
+				180
+			]
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-  componentDidMount() {
-    this.applyColor();
-  }
+	componentDidMount() {
+		this.applyColor();
+	}
 
-  componentDidUpdate(prevProps, prevState) {
-    this.applyColor();
-  }
+	componentDidUpdate(prevProps, prevState) {
+		this.applyColor();
+	}
 
-  formatColor(any) {
-    return 'rgb(' +any.join(',') + ')';
-  }
+	formatColor(any) {
+		return 'rgb(' + any.join(',') + ')';
+	}
 
-  isLight() {
-    const rgb = this.state.color;
-    return rgb.reduce((a,b) => a + b) < 127 * 3;
-  }
+	isLight() {
+		const rgb = this.state.color;
+		return rgb.reduce((a, b) => a + b) < 127 * 3;
+	}
 
-  applyColor() {
-    const color = this.formatColor(this.state.color);
-    document.body.style.background = color;
-  }
+	applyColor() {
+		const color = this.formatColor(this.state.color);
+		document.body.style.background = color;
+	}
 
-  chooseColor() {
-    const random = [];
-    for (let i = 0; i < 3; i++) {
-      random.push(Math.floor(Math.random() * 256));
-    }
-    return random;
-  }
+	chooseColor() {
+		const random = [];
+		for (let i = 0; i < 3; i++) {
+			random.push(Math.floor(Math.random() * 256));
+		}
+		return random;
+	}
 
-  handleClick() {
-    this.setState({
-      color: this.chooseColor()
-    });
-  }
-    
-  render() {
+	handleClick() {
+		this.setState({
+			color : this.chooseColor()
+		});
+	}
 
-    return (
-      <div className='center'>
-        <h1 className = {this.isLight() ? 'text-white' : 'text-dark'}>
-          The color chosen is {this.formatColor(this.state.color)}
-        </h1>
-        <Button onClick={this.handleClick} light={this.isLight()}/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='center'>
+				<h1 className={this.isLight() ? 'text-white' : 'text-dark'}>
+					The color randomly picked color is: {this.formatColor(this.state.color)}
+				</h1>
+				<Button onClick={this.handleClick} light={this.isLight()} />
+			</div>
+		);
+	}
 }
 
 export default App;
